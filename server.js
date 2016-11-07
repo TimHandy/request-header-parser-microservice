390e6655554ba44df0c4127ab3e001eb2aed6faf
Tim Handy <tjhandy@gmail.com>
2016-11-07 13:21:57 +0000

    Format output for Windows and Linux with regex

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var http = require('http');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-//app.enable('trust proxy')
+// app.enable('trust proxy')
 
-// this works! 
+// this works as a workaround?
 // http.get('http://bot.whatismyipaddress.com', function(res){
 //     res.setEncoding('utf8');
 //     res.on('data', function(chunk){
@@ -16,13 +16,11 @@ app.use(express.static('public'));
 //     });
 // });
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
     //   let ip = req.headers['x-forwarded-for']
 
     // let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-
-    //x-appengine-user-ip
 
     let ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress.split(":").pop()
@@ -30,36 +28,25 @@ app.get("/", function (req, res) {
     let language = req.headers['accept-language'].split(',')[0]
 
     let osString = req.headers['user-agent']
-    let os = (osString.split(' ')[1] + ' ' + osString.split(' ')[2] + ' ' + osString.split(' ')[3]).replace('(', '').replace(')', '')
+    let os = osString.match(/\(([^\)]+)\)/)[0].replace(/[\(|\)]+/g, '')
+    console.log('os' + os)
+    // let os = (osString.split(' ')[1] + ' ' + osString.split(' ')[2] + ' ' + osString.split(' ')[3]).replace('(', '').replace(')', '')
+
 
     let headers = req.headers
 
-    //console.log('headers: ' + headers)
-    //console.log(req)
-    console.log(req.headers)
+    // console.log(req.headers)
 
-    console.log(ip)
+    // console.log(ip)
+
     let json = {
         "ipaddress": ip,
         "language": language,
         "software": os
     }
 
-
     res.send(JSON.stringify(json))
 });
-
-app.get("/dreams", function (request, response) {
-    response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-    dreams.push(request.query.dream);
-    response.sendStatus(200);
-});
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
